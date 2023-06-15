1e86bb0d6e72af79e2b7f22ab555f6b9e9e94e26
goose <anrobbins18@gmail.com>
2023-06-15 10:45:45 -0400

    updated category and product routes for GET find all requests

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,9 @@ router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const CategoryData = await Category.findAll();
+    const CategoryData = await Category.findAll({
+      include: [{ model: Product }]
+    });
     res.status(200).json(CategoryData);
   } catch (err) {
     res.status(500).json(err);
